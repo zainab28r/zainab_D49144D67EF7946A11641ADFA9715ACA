053060f7ca5ec8a85a451d33b2207671287c2f2a
zainab28r <144320404+zainab28r@users.noreply.github.com>
2023-09-07 13:05:27 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,7 @@
+#to find the leap year
+y=2000
+if(y%4==0):
+  print("{} is leap year".format(y))
+else:
+  print("{}is not a leap year".format(y))
+  
---
+++ main.py
@@ -0,0 +1,11 @@
+#Finding Factorial
+def fact(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * fact(n - 1)
+
+
+number = 5
+result = fact(number)
+print("The factorial of {} is {} ".format(number, result))
